eafe9e88b93e2da9c7ec165f5c60aee843b3d5cb
polonkoev <polonkoev.temerlan@gmail.com>
2022-09-29 14:48:44 +0600

    second

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const divGallery = document.querySelector(".gallery");
+
+
+const markupEl = galleryItems.map(({preview, original, description })=>
+     (`<li><a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a></li>
+          `)).join('');
+
+
+divGallery.insertAdjacentHTML('afterbegin', markupEl)
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt',
+captionDelay: 250, })
